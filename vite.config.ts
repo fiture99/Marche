8524ba2d53ad88818b419a50c02b55babc29f510
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2025-09-24 13:05:12 +0000

    sdfs

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-// // 
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Marche/', // <-- Add this for GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
