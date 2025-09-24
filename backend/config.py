06a8139c49f41d064bfaffcf2dc4c32df4eee792
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2025-09-24 14:37:58 +0000

    sd

--- backend/config.py
+++ backend/config.py
@@ -23,7 +23,7 @@ class Config:
         'http://localhost:5173', 
         'http://localhost:3000',
         'http://127.0.0.1:5173',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
         'https://fiture99.github.io',
         'https://fiture99.github.io/Marche'
     ]
